1779376194534e50cc45e42501a6b54bcb7a2caf
Prajyot Dilip Raut <130274539+PrajyotRaut@users.noreply.github.com>
2023-10-12 21:07:43 +0530

    Add files via upload

---
+++ Smallest_no.java
@@ -0,0 +1,26 @@
+
+public class Smallest_no 
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 20;
+		int c = 5;
+		
+		
+		if(a<b && a<c)
+		{
+			System.out.println("a is smaller");
+		}
+		if(b<a && b<c)
+		{
+			System.out.println("b is smaller");
+		}
+		if(c<a && c<b)
+		{
+			System.out.println("c is smaller");
+		}
+		
+	}
+
+}
