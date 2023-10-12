dfe766c0bfc95baab1031fbcf5322a3586cb33f4
Prajyot Dilip Raut <130274539+PrajyotRaut@users.noreply.github.com>
2023-10-12 22:03:06 +0530

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,20 +1,43 @@
-package College;
-
 public class Car 
 {
-	String Model = "Mahindra";
-	int Price = 1500000;
+	private int year;
+	private String make;
+	private double speed;
 	
-	public void start()
+	public Car(int year, String make, double speed)
 	{
-		System.out.println("The Car has started");
+		this.year = year;
+		this.make = make;
+		this.speed = speed;
 	}
-	public void stop()
+
+	public int getYear() 
 	{
-		System.out.println("The Car has stopped");
+		return year;
 	}
-	public void move()
+
+	public String getMake() 
 	{
-		System.out.println("The Car is moving");
-	}		
+		return make;
+	}
+
+	public double getSpeed() 
+	{
+		return speed;
+	}
+	
+	public static void main(String[] args)
+	{
+		Car c1 = new Car(2015,"Honda",85.0);
+		System.out.println("The year of car is : " + c1.year);
+		System.out.println("The make of car is : " + c1.make);
+		System.out.println("The speed of car is : " + c1.speed);
+		
+		c1.getYear();
+		c1.getMake();
+		c1.getSpeed();
+	}
+
+	
+
 }
